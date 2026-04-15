64e7affc0fe944f5873286561ce73360ed80106f
Esse <esefapohunda@Gmail.com>
2026-04-15 15:56:27 -0300

    feat: add Google Analytics integration with gtag.js for enhanced tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -94,6 +94,20 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           }}
         />
         {/* End Google Tag Manager */}
+        {/* Google tag (gtag.js) */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-TJLK8R1JYN"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-TJLK8R1JYN');`,
+          }}
+        />
       </head>
       <body className="antialiased">
         <noscript>
